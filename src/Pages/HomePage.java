79bf0fa45b874e94ae0c705f46bccf74be99ce1b
umaanantha77 <umaanantha77@gmail.com>
2024-02-08 18:03:13 -0600

    changes added

--- src/Pages/HomePage.java
+++ src/Pages/HomePage.java
@@ -3,7 +3,14 @@ package Pages;
 public class HomePage {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		int i =1;
+		System.out.println("test" +i);
+		i++;
+		System.out.println("test" +i);
+		i++;
+		System.out.println("test" +i);
+		i++;
+		System.out.println("test" +i);
 
 	}
 
